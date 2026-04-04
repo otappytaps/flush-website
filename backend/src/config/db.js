a1a8eb3f6f2357523458f1ed2fd01d55c34b7fcf
imnotneon-dev <jenrick_lim@dlsu.edu.ph>
2026-04-04 20:36:39 +0800

    push: converted to mongodb atlas

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose"
+import dns from "node:dns/promises"
+
+dns.setServers(["8.8.8.8"]);
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/flushDB");
+        const DB_URL = "mongodb+srv://jenricklim_db_user:admin123456@flushcluster.zdndybn.mongodb.net/flushapp?appName=FlushCluster"
+        await mongoose.connect(DB_URL);
         console.log("MONGODB CONNECTED SUCCESSFULLY!")
     } catch (error) {
         console.error("MONGODB ERROR!", error);
