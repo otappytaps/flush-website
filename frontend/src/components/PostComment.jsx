80d4ab85622ea9da504de84bc2046a359d959536
otappytaps <coadrian812@gmail.com>
2026-04-07 23:48:02 +0800

    fixed minor issues

--- frontend/src/components/PostComment.jsx
+++ frontend/src/components/PostComment.jsx
@@ -258,7 +258,7 @@ function PostComment({ post, refreshPost, comment, user }) {
           </button>
           <span className="comment-dislike-count">{comment.dislikes}</span>
 
-          {user.username || user.username === comment.author ? (
+          {user || user.username === comment.author ? (
             <>
               <button
                 className="delete-comment-btn"
